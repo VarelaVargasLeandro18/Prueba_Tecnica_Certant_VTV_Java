96a2c0a63492550b615072b304026b125b777c18
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-08-10 16:18:55 -0300

    JPAEntityManagerFactory Creado y funcionando.

---
+++ Prueba_Tecnica_VTV/src/main/java/com/mycompany/prueba_tecnica_vtv/JPAEntityManagerFactory.java
@@ -0,0 +1,37 @@
+package com.mycompany.prueba_tecnica_vtv;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author bapo
+ */
+public class JPAEntityManagerFactory {
+    
+    private static EntityManagerFactory emf = buildFactory();
+    private static EntityManager em = null;
+    
+    private static final String PERSISTENCE_UNIT_NAME = "Prueba_Tecnica_VTV";
+    
+    private static EntityManagerFactory buildFactory() {
+        
+        try {
+            return Persistence.createEntityManagerFactory( PERSISTENCE_UNIT_NAME );
+        }
+        catch ( Throwable ex ) {
+            throw new RuntimeException(ex);
+        }
+        
+    }
+    
+    public static EntityManager getEntityManager() {
+        
+        if ( em == null || !em.isOpen() )
+            em = emf.createEntityManager();
+                
+        return em;
+    }
+    
+}
