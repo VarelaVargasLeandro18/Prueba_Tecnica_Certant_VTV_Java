65e6e49e4614e22e65ffa133c4464cf6f6325198
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-08-12 11:07:46 -0300

    Main sin instrucciones.

--- Prueba_Tecnica_VTV/src/main/java/com/mycompany/prueba_tecnica_vtv/Main.java
+++ Prueba_Tecnica_VTV/src/main/java/com/mycompany/prueba_tecnica_vtv/Main.java
@@ -1,8 +1,5 @@
 package com.mycompany.prueba_tecnica_vtv;
 
-import model.CRUD.abstractCRUD.AbstractCRUD;
-import model.personas.Inspector;
-
 /**
  *
  * @author Varela Vargas Leandro Gastón
@@ -13,8 +10,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception { 
-        JPAEntityManagerFactory.getEntityManager();
-        AbstractCRUD<Inspector,Long> i = new AbstractCRUD(Inspector.class);
     }
     
 }
