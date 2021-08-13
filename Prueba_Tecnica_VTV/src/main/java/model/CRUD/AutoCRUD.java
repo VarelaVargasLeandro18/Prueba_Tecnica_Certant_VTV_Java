b48b73efc799efb7bec99bb0967373d8929bd1df
VarelaVargasLeandro18 <varelavargasleandro@outlook.com.ar>
2021-08-13 12:17:13 -0300

    Creacion de Informes.

--- Prueba_Tecnica_VTV/src/main/java/model/CRUD/AutoCRUD.java
+++ Prueba_Tecnica_VTV/src/main/java/model/CRUD/AutoCRUD.java
@@ -1,7 +1,10 @@
 package model.CRUD;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import model.Auto;
 import model.CRUD.abstractCRUD.AbstractCRUD;
+import model.CRUD.abstractCRUD.ReadEntityException;
 
 /**
  *
@@ -11,6 +14,33 @@ public final class AutoCRUD extends AbstractCRUD<Auto, String> {
 
     public AutoCRUD() {
         super(Auto.class);
+    }
+    
+    
+    /**
+     * Cuato Informe - Chequeo de fiestas de vencimiento dentro del año de haberse
+     * ejecutado una Inspeccion o que no esté aprobada.
+     * @param aprobado Valor de String de un estado aprobado.
+     * @return 
+     * @throws model.CRUD.abstractCRUD.ReadEntityException 
+     */
+    public List<Auto> chequeoVencimiento (String aprobado) throws ReadEntityException {
+        
+        try {
+            String query = "SELECT i.auto FROM Inspeccion i "
+                    + "WHERE i.Id != (SELECT i.Id "
+                    + "FROM Inspeccion i WHERE i.estado.estado = :aprobado "
+                    + "GROUP BY i.auto "
+                    + "HAVING i.fecha < :unanioatras);";
+            return this.getEntityManager()
+                    .createQuery(query, Auto.class)
+                    .setParameter("aprobado", aprobado)
+                    .setParameter("unanioatras", LocalDateTime.now().minusYears(1l))
+                    .getResultList();
+        } catch(Throwable ex) {
+            throw new ReadEntityException(ex);
+        }
+        
     }
     
 }
--- Prueba_Tecnica_VTV/src/main/java/model/CRUD/InspeccionCRUD.java
+++ Prueba_Tecnica_VTV/src/main/java/model/CRUD/InspeccionCRUD.java
@@ -1,7 +1,12 @@
 package model.CRUD;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 import model.CRUD.abstractCRUD.AbstractCRUD;
+import model.CRUD.abstractCRUD.ReadEntityException;
 import model.inspeccion.Inspeccion;
+import model.personas.Propietario;
 
 /**
  *
@@ -11,6 +16,63 @@ public final class InspeccionCRUD extends AbstractCRUD<Inspeccion, Long> {
     
     public InspeccionCRUD() {
         super(Inspeccion.class);
+    }
+    
+    /**
+     * Primer Informe - Obtención de Lista de las Inspecciones en la última semana,
+     * es decir, desde hace siete días del día de la fecha hasta hoy.
+     * @return List inspecciones en la semana.
+     * @throws model.CRUD.abstractCRUD.ReadEntityException
+     */
+    public List<Inspeccion> obtenerInspeccionesDeLaSemana() throws ReadEntityException {
+        try {
+            String query = "SELECT I FROM Inspeccion I WHERE I.fecha < :ahora AND I.fecha > :sietediasatras;";
+            return this.getEntityManager().createQuery(query, Inspeccion.class)
+                    .setParameter("ahora", LocalDate.now())
+                    .setParameter("sevendaysago", LocalDateTime.now().minusDays(7l))
+                    .getResultList();
+        } catch ( Throwable ex ) {
+            throw new ReadEntityException(ex);
+        }
+    }
+    
+    /**
+     * Segundo Informe Pt2 - Obtención de Lista de Inspecciones en los últimos tres días.
+     * @return List inspecciones en los últimos tres días.
+     * @throws model.CRUD.abstractCRUD.ReadEntityException
+     */
+    public List<Inspeccion> obtenerInspeccionesUltimosTresDias() throws ReadEntityException {
+        try {
+            String query = "SELECT I FROM Inspeccion I WHERE I.fecha >= :tresdiasantes";
+            return this.getEntityManager()
+                    .createQuery(query, Inspeccion.class)
+                    .setParameter("tresdiasantes", LocalDateTime.now().minusDays(3l))
+                    .getResultList();
+        } catch ( Throwable ex ) {
+            throw new ReadEntityException(ex);
+        }
+    }
+    
+    /**
+     * Tercer Informe Pt2 - Obtención de Inspecciones realizadas a los autos de una persona con más de un auto.
+     * @param p Propietario cuyas inspecciones se quieren conocer
+     * @return List inspecciones de un dueño si tiene más de tres autos. Empty si solo tiene uno.
+     */
+    public List<Inspeccion> obtenerInspeccionesDePersonaSiTieneMasDeUnAuto( Propietario p ) throws ReadEntityException {
+        try {
+            String query = "SELECT i FROM Inspeccion i"
+                    + " WHERE i.auto.propietario = :persona "
+                    + "AND "
+                    + "1 < (SELECT COUNT(a.propietario.CUIL) "
+                    + "FROM Auto a "
+                    + "GROUP BY a.propietario.CUIL)";
+            return this.getEntityManager()
+                    .createQuery(query, Inspeccion.class)
+                    .setParameter(":persona", p)
+                    .getResultList();
+        } catch ( Throwable ex ) {
+            throw new ReadEntityException(ex);
+        }     
     }
     
 }
